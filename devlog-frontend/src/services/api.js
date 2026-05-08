74dc438ff9ee973abcedc2f40d5e8f383116f760
Lohar Parth Sunil <155839780+PhantomCodeGhost@users.noreply.github.com>
2026-05-08 17:22:09 +0530

    Update api.js

--- devlog-frontend/src/services/api.js
+++ devlog-frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const api = axios.create({
 
-    baseURL: "http://localhost:8080/api",
+    baseURL: "https://devlogs-j594.onrender.com",
 
     headers: {
         "Content-Type": "application/json",
